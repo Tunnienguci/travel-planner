49b50a9d987cfec819fa9bc4ce3053c474be87b1
Tuan Quach Cong <tuanqc0712@gmail.com>
2025-03-03 22:00:29 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ExpensesComponent } from './common/expenses/expenses.component';
+import { TicketComponent } from './common/ticket/ticket.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        data: { title: '' }
+    },
+    {
+        path: 'expenses',
+        component: ExpensesComponent,
+        data: { title: 'Trip Expenses' }
+    },
+    {
+        path: 'ticket',
+        component: TicketComponent,
+        data: { title: 'My Ticket' }
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        data: { title: 'My Profile' }
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+];
